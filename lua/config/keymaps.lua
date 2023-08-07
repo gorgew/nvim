142dca915282b9058d0de3bace6f2a76997279af
gorgew <78799838+gorgew@users.noreply.github.com>
2023-08-06 21:44:25 -0700

    configuring + keymaps progress

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,24 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    if opts.remap and not vim.g.vscode then
+      opts.remap = nil
+    end
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+-- vim.keymap.set("i", "hj", "<Esc>", { noremap = true })
+map("i", "hj", "<ESC>")
+
+-- Helix influenced keymaps
+-- Helix goto
+map("n", "gl", "$", { remap = true })
+map("n", "ge", "G", { remap = true })
